224cd392f05f9290954e4632adc6a34c26eb877e
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2022-02-11 17:47:29 +0700

    make request validation for Schedule modul

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ScheduleRequest;
+use App\Models\Classroom;
+use App\Models\Schedule;
+use App\Models\Study;
+use App\Models\Teacher;
+use Yajra\DataTables\Facades\DataTables;
 
 class ScheduleController extends Controller
 {
-    //
+    public function index()
+    {
+        if (request()->ajax()) {
+            $classrooms = Classroom::latest()->get();
+            return DataTables::of($classrooms)
+                    ->addIndexColumn()
+                    ->addColumn('action', function($row){
+                        $btn =
+                        '<div class="d-flex justify-content-center">
+                            <a href="javascript:void(0)" data-id="'.$row->id.'" id="showSchedule" class="btn btn-sm btn-primary mr-2">
+                            <i class="fas fa-eye"></i>
+                            Jadwal
+                            </a>
+                        </div>';
+
+                        return $btn;
+                    })
+                    ->rawColumns(['action'])
+                    ->make(true);
+        }
+
+        return view('schedules.index', [
+            'title' => 'Jadwal',
+        ]);
+    }
+
+    public function create()
+    {
+        return view('schedules.create', [
+            'title' => 'Tamvah Jadwal',
+            'schedule' => new Schedule(),
+            'classrooms' => Classroom::all(),
+            'teachers' => Teacher::all(),
+            'studies' => Study::all(),
+        ]);
+    }
+
+    public function store(ScheduleRequest $request)
+    {
+        Schedule::create([
+
+        ]);
+    }
 }
---
+++ resources/views/schedules/index.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.app', compact('title'))
+
+@section('content')
+@include('sweetalert::alert')
+
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+    <div class="row mb-2">
+        <div class="col-sm-6">
+        <h1 class="m-0">{{ $title ?? '' }}</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item active">{{ Breadcrumbs::render('schedules') }}</li>
+        </ol>
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<div class="container-fluid mb-3 d-flex justify-content-end">
+    <div class="row">
+        <div class="col-12">
+            {{-- @can('item-create') --}}
+                <a href="{{ route('schedules.create') }}" class="btn btn-sm btn-primary">Tambah</a>
+            {{-- @endcan --}}
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    @include('components.alerts')
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title">Data Jadwal</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+            <table id="data-table" class="table table-bordered table-striped">
+                <thead class="table-dark">
+                    <tr>
+                        <th style="width: 1%">No.</th>
+                        <th>Nama</th>
+                        <th class="text-center"><i class="fas fa-cogs"></i></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+</div>
+
+@endsection
+
+@section('custom-styles')
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('asset')}}/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="{{ asset('asset')}}/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="{{ asset('asset')}}/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+@endsection
+@section('custom-scripts')
+
+    <!-- DataTables  & Plugins -->
+    <script src="{{ asset('asset')}}/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('asset')}}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="{{ asset('asset')}}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="{{ asset('asset')}}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="{{ asset('asset')}}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="{{ asset('asset')}}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="{{ asset('asset')}}/plugins/jszip/jszip.min.js"></script>
+    <script src="{{ asset('asset')}}/plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="{{ asset('asset')}}/plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="{{ asset('asset')}}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="{{ asset('asset')}}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="{{ asset('asset')}}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
+    <script>
+        $(function () {
+
+            let table = $('#data-table').DataTable({
+                processing: true,
+                serverSide: true,
+                responsive: true,
+
+                ajax: "{{ route('schedules.index') }}",
+                columns: [
+                    {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+                    {data: 'name', name: 'name'},
+                    {data: 'action', name: 'action', orderable: true, searchable: true},
+                ]
+            });
+        });
+    </script>
+
+@endsection
