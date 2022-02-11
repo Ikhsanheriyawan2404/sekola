1d82262766ce84c0c5b9768b635061f721ba4b23
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2022-02-11 16:59:38 +0700

    schedules relationing to some tables

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['study_id', 'classroom_id', 'room_id', 'teacher_id', 'start', 'end'];
+
+    public function study()
+    {
+        return $this->belongsTo(Study::class);
+    }
+
+    public function classroom()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
 }
