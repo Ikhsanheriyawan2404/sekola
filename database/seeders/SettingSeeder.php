dc56bcd33299de6b122c3d46b386e58d104dea32
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2022-02-13 23:39:57 +0700

    create data school info migration

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Seeder;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Setting::create([
+            'school_name' => 'SMK N 1 SUZURAN',
+            'address' => 'Desa Kempek Blok Penangisan Kecamatan Gempol Kabupaten Cirebon Jawa Barat 45161',
+            'phone' => '082117088123',
+            'email' => 'ikhsanheriyawan2404@gmail.com',
+            'type' => 'Swasta',
+        ]);
+    }
+}
