9208d8fab97d9811f2ab21550c6889841f86d94c
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2022-02-14 19:49:13 +0700

    modif teacher n student seeders

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Student;
+use App\Models\{User, Student};
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class StudentSeeder extends Seeder
 {
@@ -14,19 +15,24 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        // $student = Student::create([
-        //     'name' => 'Ikhsan Heriyawan',
-        //     'nisn' => '240416',
-        //     'gender' => 'L',
-        //     'religion' => 'Islam',
-        //     'classroom_id' => 1,
-        //     'date_of_birth' => '2001-02-19',
-        //     'phone' => '082117088123',
-        //     'email' => 'ikhsan@gmail.com',
-        // ]);
+        $student = Student::create([
+            'name' => 'Ikhsan Heriyawan',
+            'nisn' => '240416',
+            'gender' => 'L',
+            'religion' => 'Islam',
+            'classroom_id' => 1,
+            'date_of_birth' => '2001-02-19',
+            'phone' => '082117088123',
+            'email' => 'ikhsan@gmail.com',
+        ]);
 
-        // User::create([
-        //     'name' => $student['name'],
-        // ]);
+        $user = User::create([
+            'name' => $student['name'],
+            'email' => $student['email'],
+            'password' => Hash::make($student['nisn']),
+            'student_id' => $student['id'],
+        ]);
+
+        $user->assignRole('Siswa');
     }
 }
--- database/seeders/TeacherSeeder.php
+++ database/seeders/TeacherSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Teacher;
+use App\Models\{Teacher, User};
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class TeacherSeeder extends Seeder
 {
@@ -17,20 +18,37 @@ class TeacherSeeder extends Seeder
         $teacher1 = Teacher::create([
             'name' => 'Sukirno',
             'nip' => '1902921021',
-            'email' => 'guru@email.com',
+            'email' => 'sukirno@mail.test',
             'phone' => '082119883288',
             'gender' => 'L',
         ]);
 
         $teacher1->studies()->sync(1);
+        $user = User::create([
+            'name' => $teacher1['name'],
+            'email' => $teacher1['email'],
+            'password' => Hash::make($teacher1['nip']),
+            'teacher_id' => $teacher1['id'],
+        ]);
+
+        $user->assignRole('Guru');
 
         $teacher2 = Teacher::create([
             'name' => 'Suherti',
-            'nip' => '1902921022',
-            'email' => 'teacher@email.com',
+            'nip' => '240416',
+            'email' => 'suherti@mail.test',
             'phone' => '082423428',
             'gender' => 'P',
         ]);
+
+        $user2 = User::create([
+            'name' => $teacher2['name'],
+            'email' => $teacher2['email'],
+            'password' => Hash::make($teacher2['nip']),
+            'teacher_id' => $teacher2['id'],
+        ]);
+
+        $user2->assignRole('Guru');
 
         $teacher2->studies()->sync(2);
     }
