7e31eb4e29985692a8958bed6f3c4fd3200ee957
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2022-02-22 20:29:12 +0700

    edit title app.name

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
   <!-- /.login-logo -->
   <div class="card card-outline card-primary">
     <div class="card-header text-center">
-      <a href="{{ asset('asset') }}/index2.html" class="h1"><b>Accounting APP</b></a>
+      <a href="{{ asset('asset') }}/index2.html" class="h1"><b>Sisfo APP</b></a>
     </div>
     <div class="card-body">
       <p class="login-box-msg">Masukan email dan password</p>
