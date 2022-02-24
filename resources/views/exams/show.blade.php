b8cc9921da39b543c708418e69d857e02129f7b6
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2022-02-24 19:18:11 +0700

    implement if the exam time is over, auto submit

--- resources/views/exams/show.blade.php
+++ resources/views/exams/show.blade.php
@@ -37,7 +37,7 @@
                     <ul class="list-group">
                         <li class="list-group-item d-flex justify-content-between align-items-center">
                             Waktu
-                            <span class="badge badge-primary badge-pill">{{ $quiz->time }}</span>
+                            <span class="badge badge-primary badge-pill js-timeout">{{ $quiz->time }}</span>
                         </li>
                         <li class="list-group-item d-flex justify-content-between align-items-center">
                             Mata Pelajaran
@@ -83,7 +83,7 @@
                 </div>
                 <!-- /.card-header -->
                 <!-- form start -->
-                <form action="{{ route('exams.store') }}" method="post">
+                <form action="{{ route('exams.store') }}" method="post" name="exam">
                 @csrf
                 <div class="card-body">
                     <div class="container-fluid">
@@ -143,5 +143,33 @@
                 $('button').attr('disabled', 'disabled');
             });
         });
+    </script>
+    <script type="text/javascript">
+        var interval;
+        var form = document.forms.exam;
+        function countdown() {
+          clearInterval(interval);
+
+          interval = setInterval( function() {
+              var timer = $('.js-timeout').html();
+              timer = timer.split(':');
+              var minutes = timer[0];
+              var seconds = timer[1];
+              seconds -= 1;
+              if (minutes < 0) return;
+              else if (seconds < 0 && minutes != 0) {
+                  minutes -= 1;
+                  seconds = 59;
+              }
+              else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
+
+              $('.js-timeout').html(minutes + ':' + seconds);
+
+              if (minutes == 0 && seconds == 0) { clearInterval(interval); form.submit(); alert("Waktu sudah habis!");}
+          }, 1000);
+        }
+
+        $('.js-timeout').text("{{ $quiz->time }}:00");
+        countdown();
     </script>
 @endsection
